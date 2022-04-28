09d07eeac8036ad668981b9e7010d4f168b6deb2
Thabo Lebelo <thabolebelo@icloud.com>
2022-04-28 19:57:39 +0200

    Deploy lambda to Test & Prod stages

--- lib/cdk-cicd-pipeline-stack.ts
+++ lib/cdk-cicd-pipeline-stack.ts
@@ -1,17 +1,31 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { ManualApprovalStep } from 'aws-cdk-lib/pipelines';
+import { PipelineAppStage } from './pipeline-app-stage';
 
 export class CdkCicdPipelineStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
 
-        new CodePipeline(this, 'Pipeline', {
+        const pipeline = new CodePipeline(this, 'Pipeline', {
             pipelineName: 'TestPipeline',
             synth: new ShellStep('Synth', {
-                input: CodePipelineSource.gitHub('thabo-lebelo/cdk-cicd-pipeline', 'main'), 
+                input: CodePipelineSource.gitHub('thabo-lebelo/cdk-cicd-pipeline', 'main'),
                 commands: ['npm ci', 'npm run build', 'npx cdk synth']
             })
         });
+
+        const testingStage = pipeline.addStage(new PipelineAppStage(this, "test", {
+            env: { account: "755314965794", region: "us-east-1" }
+        }));
+
+
+        testingStage.addPre(new ShellStep("Run Unit Tests", { commands: ['npm install', 'npm test'] }));
+        testingStage.addPost(new ManualApprovalStep('Manual approval before production'));
+
+        const prodStage = pipeline.addStage(new PipelineAppStage(this, "prod", {
+            env: { account: "755314965794", region: "us-east-1" }
+        }));
     }
 }
