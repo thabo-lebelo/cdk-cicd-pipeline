934744e860dc9dacc4f3aa8cb274e52103dc4bbe
Thabo Lebelo <thabolebelo@icloud.com>
2022-04-28 19:58:09 +0200

    Deploy lambda to Test & Prod stages

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export async function handler(event: string, context: string) {
+    console.log('Stage Name is: ' + process.env.stage);
+    return {
+        body: 'Hello from a Lambda Function 👋',
+        statusCode: 200,
+    };
+}
